43b434013a6edb4318af9e905b04cf0bad595fc6
Sonia <sonia.gummuluru@gmail.com>
2020-05-12 22:39:03 -0500

    Created deleteUser, :id , addUser endpoints

--- server/server.js
+++ server/server.js
@@ -9,6 +9,50 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
+var user = {
+    "user4" : {
+       "name" : "mohit",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+    }
+ }
+ 
+ app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( "data" + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
+    });
+ })
+
+
+ app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( "data" + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+ 
+
+ var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+   // First read existing users.
+   fs.readFile("data" + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      delete data["user" + 2];
+       
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
 var server = app.listen(8005, function () {
       console.log("Server running at http://localhost:%s", 8005)
 })
